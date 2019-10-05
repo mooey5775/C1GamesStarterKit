fdbf9436bc4b9cb8ad3e2474ef9303f6a3b223a2
Edward Li <edwardyaoli@gmail.com>
2019-10-05 14:05:44 -0400

    bugfixes

--- new-algo/algo_strategy.py
+++ new-algo/algo_strategy.py
@@ -45,7 +45,6 @@ class AlgoStrategy(gamelib.AlgoCore):
 
         self.front_filters = [[0, 13], [1, 13], [26, 13], [27, 13], [3, 12], [24, 12], [5, 11], [22, 11]]
         self.maze_encryptors = [[8, 8], [9, 8], [10, 8], [11, 8], [12, 8], [13, 8], [14, 8], [15, 8], [16, 8], [17, 8], [18, 8], [19, 8], [7, 6], [9, 6], [10, 6], [11, 6], [12, 6], [13, 6], [14, 6], [15, 6], [16, 6], [17, 6], [18, 6], [20, 6], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5], [17, 5], [10, 3], [12, 3], [13, 3], [14, 3], [15, 3], [17, 3], [13, 2], [14, 2]]
-        self.maze_init
         self.mazeL = [[7, 7], [17, 4], [12, 1]]
         self.mazeR = [[20, 7], [10, 4], [15, 1]]
         self.maze_switch = False
@@ -148,9 +147,9 @@ class AlgoStrategy(gamelib.AlgoCore):
 
         deploy_locations = []
 
-        if (self.is_maze_on_L or (opponent_bits > 10 and self_bits > 7)):
+        if (self.maze_on_L or (opponent_bits > 10 and self_bits > 7)):
             deploy_locations.extend(self.filter_blocked_locations(left_scrambler_pts, game_state))
-        if (not self.is_maxe_on_L or (opponent_bits > 10 and self_bits > 7)):
+        if (not self.maze_on_L or (opponent_bits > 10 and self_bits > 7)):
             deploy_locations.extend(self.filter_blocked_locations(right_scrambler_pts, game_state))
 
         # While we have remaining bits to spend lets send out scramblers randomly.
@@ -163,9 +162,9 @@ class AlgoStrategy(gamelib.AlgoCore):
         left_loc = [13, 0]
         right_loc = [14, 0]
 
-        spawn_loc = left_loc if self.is_maze_on_L else right_loc
+        spawn_loc = left_loc if self.maze_on_L else right_loc
 
-        _, damage = least_damage_spawn_location(game_state, [spawn_loc])
+        _, damage = self.least_damage_spawn_location(game_state, [spawn_loc])
         self_bits = game_state.get_resource(game_state.BITS, player_index=0)
 
         num_emps = round(self_bits / 21)
